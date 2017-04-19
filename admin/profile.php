25d672ecc8f6b428d7c7e1ccc0d1d03f24781734
Manuel Wiltz <manuelwiltz@gmail.com>
2017-04-19 13:43:57 +0200

    fixing profile issues

--- admin/profile.php
+++ admin/profile.php
@@ -14,7 +14,6 @@ if (!isset($_SESSION['userid'])) {
         <title>Admin | Themes</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link href="style.css" rel="stylesheet">
-        <script src="http://cdn.ckeditor.com/4.6.1/standard/ckeditor.js"></script>
     </head>
     <body>
 
@@ -59,11 +58,63 @@ if (!isset($_SESSION['userid'])) {
 
                                 <div class="row">
                                     <div class="col-md-12 padding-sm">
-                                        
+                                        <h3>Hello, <?php echo $username; ?></h3>
                                     </div>
                                 </div>
 
                                 <hr class="medium">
+
+                                <?php
+                                $statement = "SELECT * FROM users where username=" . $username . ";";
+
+                                if ($res = $conn->query($statement)) {
+                                    if ($res->row_nums = 1) {
+                                        $row = $res->fetch_assoc();
+                                        $firstname = $row['firstname'];
+                                        $lastname = $row['lastname'];
+                                        $username = $row['username'];
+                                        $email = $row['email'];
+                                        $password = $row['password'];
+                                    }
+                                } else {
+                                    $row = $res->fetch_assoc();
+                                    $firstname = "";
+                                    $lastname = "";
+                                    $username = "";
+                                    $email = "";
+                                    $password = "";
+                                }
+                                ?>
+
+                                <div class="row padding-md">
+                                    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                                        <div class="form-group">
+                                            <label for="text">Firstname:</label>
+                                            <input type="text" class="form-control" name="firstname" value="<?php echo $firstname; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="text">Lastname:</label>
+                                            <input type="text" class="form-control" name="lastname" value="<?php echo $lastname; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="text">Username:</label>
+                                            <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email">Email:</label>
+                                            <input type="email" class="form-control" name="email" value="<?php echo $email; ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="pwd">Password:</label>
+                                            <input type="password" class="form-control" name="password1" >
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="pwd">Repeat password:</label>
+                                            <input type="password" class="form-control" name="password2" >
+                                        </div>
+                                        <button type="submit" class="btn btn-default btn-green">Update Profile</button>
+                                    </form>
+                                </div>
 
                                 <br>
 
