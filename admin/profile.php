2d8bff7722bf7ef3812905e1dc245f08501a221a
Manuel Wiltz <manuelwiltz@gmail.com>
2017-04-19 22:27:55 +0200

    bug fixing

--- admin/profile.php
+++ admin/profile.php
@@ -56,37 +56,46 @@ if (!isset($_SESSION['userid'])) {
                             </div>
                             <div class="panel-body">
 
-                                <div class="row">
-                                    <div class="col-md-12 padding-sm">
-                                        <h3>Hello, <?php echo $username; ?></h3>
-                                    </div>
-                                </div>
-
-                                <hr class="medium">
-
                                 <?php
-                                $statement = "SELECT * FROM users where username=" . $username . ";";
+                                $statement = "SELECT * FROM users where username = '" . $username . "';";
 
                                 if ($res = $conn->query($statement)) {
-                                    if ($res->row_nums = 1) {
+                                    if ($res->num_rows > 0) {
                                         $row = $res->fetch_assoc();
+
                                         $firstname = $row['firstname'];
                                         $lastname = $row['lastname'];
                                         $username = $row['username'];
                                         $email = $row['email'];
                                         $password = $row['password'];
+                                        $joined = $row['create_date'];
                                     }
                                 } else {
-                                    $row = $res->fetch_assoc();
                                     $firstname = "";
                                     $lastname = "";
                                     $username = "";
                                     $email = "";
                                     $password = "";
+                                    $joined = "Unknown";
                                 }
                                 ?>
 
+                                <div class="row">
+                                    <div class="col-md-12 padding-sm">
+                                        <h3>Hello, <?php echo $username; ?></h3>
+                                        <p class="small" style="color: #777">You joined:  <?php echo $joined; ?></p>
+                                    </div>
+                                </div>
+
+                                <hr class="medium">
+
                                 <div class="row padding-md">
+                                    <h4 class="bold">Change your profile data:</h4>
+
+                                    <?php
+                                    // TODO: validate input
+                                    ?>
+
                                     <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
                                         <div class="form-group">
                                             <label for="text">Firstname:</label>
@@ -104,15 +113,68 @@ if (!isset($_SESSION['userid'])) {
                                             <label for="email">Email:</label>
                                             <input type="email" class="form-control" name="email" value="<?php echo $email; ?>">
                                         </div>
-                                        <div class="form-group">
-                                            <label for="pwd">Password:</label>
-                                            <input type="password" class="form-control" name="password1" >
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="pwd">Repeat password:</label>
-                                            <input type="password" class="form-control" name="password2" >
-                                        </div>
                                         <button type="submit" class="btn btn-default btn-green">Update Profile</button>
+                                    </form>
+                                </div>
+
+                                <hr class="medium">
+
+                                <div class="row padding-md">
+                                    <h4 class="bold">Change your password:</h4>
+
+                                    <?php
+                                    // TODO: validate new password
+
+                                    if (isset($_POST['passwordOld']) && isset($_POST['passwordNew1']) && isset($_POST['passwordNew2'])) {
+
+                                        $errors = [];
+
+                                        $passwordOld = md5($conn->real_escape_string(htmlspecialchars(stripcslashes(trim($_POST['passwordOld'])))));
+                                        $passwordNew1 = md5($conn->real_escape_string(htmlspecialchars(stripcslashes(trim($_POST['passwordNew1'])))));
+                                        $passwordNew2 = md5($conn->real_escape_string(htmlspecialchars(stripcslashes(trim($_POST['passwordNew2'])))));
+
+                                        $statement = "SELECT * FROM users where username = '" . $username . "';";
+                                        if ($result = $conn->query($statement)) {
+                                            if ($result->num_rows == 1) {
+
+                                                if (($passwordNew1 === $passwordNew2) && ($passwordOld === $password)) {
+                                                    
+                                                } else {
+                                                    array_push($errors, 'New passwords or old do not match! Please cheack your input!');
+                                                }
+                                            }
+                                        }
+
+                                        if (count($errors) == 0) {
+                                            $statement = "UPDATE users SET password = '" . $passwordNew1 . "' WHERE username = '" . $username . "';";
+
+                                            if ($result = $conn->query($statement)) {
+                                                echo '<script>window.location.replace("profile.php");</script>';
+                                            }
+                                        } else {
+                                            echo '<div class="row padding-md">';
+                                            foreach ($errors as $value) {
+                                                echo '<p class="bold" style="color: red;">' . $value . '</p>';
+                                            }
+                                            echo '</div>';
+                                        }
+                                    }
+                                    ?>
+
+                                    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                                        <div class="form-group">
+                                            <label for="pwd">Old password:</label>
+                                            <input type="password" class="form-control" name="passwordOld" placeholder="Enter old password">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="pwd">New password:</label>
+                                            <input type="password" class="form-control" name="passwordNew1" placeholder="Enter new password">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="pwd">Repeat new password:</label>
+                                            <input type="password" class="form-control" name="passwordNew2" placeholder="Repeat new password">
+                                        </div>
+                                        <button type="submit" class="btn btn-default btn-green">Change Password</button>
                                     </form>
                                 </div>
 
